b6ab550de52156167c70821572572298b702857a
marcosarmandomardones-ops <marcosarmandomardones@gmail.com>
2025-11-07 14:30:21 -0300

    3-5 Módulo
    Agregado de funciones (Módulos)

--- proyecto-paises/Main.py
+++ proyecto-paises/Main.py
@@ -105,8 +105,203 @@ def actualizar_datos():
     ejemplares = int(ejemplares)
 
     
+# funcion para buscar paises (3)
+def buscar_paises():
+    print("\n------ Buscar Países ------")
+    nombre_pais = input("Ingrese parte o el nombre completo del país: ").strip().lower()
+    
+    if nombre_pais == "":
+        print("Error al ingresar el nombre del país")
+        return
+    
+    lista_paises = obtener_datos()
+    lista_resultado = []
+
+    # Recorremos la lista de paises
+    for pais in lista_paises:
+        nombre = pais["nombre"].lower()
+
+        if nombre_pais in nombre:   # por coincidencia parcial
+            lista_resultado.append(pais)
+    
+    if len(lista_resultado) == 0:
+        print("No se encontraron resultados en la búsqueda")
+        return
+
+    # mostrar resultados de la busqueda 
+    print("\nResultados encontrados:")
+    print("---------------------------")
+    for pais in lista_resultado:
+        print(f"Nombre: {pais['nombre'].capitalize()}")
+        print(f"Población: {pais['poblacion']}")
+        print(f"Superficie: {pais['superficie']}")
+        print(f"Continente: {pais['continente'].capitalize()}")
+        print("---------------------------")
 
 
+
+
+# Función para filtrar países (4)
+def filtrar_paises():
+    print("\n------ Filtrar Países ------")
+    print("1. Filtrar por continente")
+    print("2. Filtrar por población mínima")
+    print("3. Filtrar por superficie mínima")
+    print("4. Volver al menú")
+
+    opcion_filtro = input("Seleccione una opción: ").strip()
+
+    lista_paises = obtener_datos()
+    lista_filtrada = []
+
+    # filtar por continente 
+    if opcion_filtro == "1":
+        continente = input("Ingrese el continente: ").strip().lower()
+
+        if continente == "":
+            print("Error al ingresar el continente")
+            return
+
+        for pais in lista_paises:
+            if pais["continente"].lower() == continente:
+                lista_filtrada.append(pais)
+
+    # filtrar por poblción minima
+    elif opcion_filtro == "2":
+        poblacion = input("Ingrese población mínima: ").strip()
+
+        if not validar_numero(poblacion):
+            print("Error al ingresar un número no válido")
+            return
+
+        poblacion = int(poblacion)
+
+        for pais in lista_paises:
+            if pais["poblacion"] >= poblacion:
+                lista_filtrada.append(pais)
+
+    # filtrar por superficie minima
+    elif opcion_filtro == "3":
+        superficie = input("Ingrese superficie mínima: ").strip()
+
+        if not validar_numero(superficie):
+            print("Error al ingresar un número no válido")
+            return
+
+        superficie = int(superficie)
+
+        for pais in lista_paises:
+            if pais["superficie"] >= superficie:
+                lista_filtrada.append(pais)
+
+    # Volver al menú
+    elif opcion_filtro == "4":
+        return
+    else:
+        print("Opción no válida")
+        return
+
+    if len(lista_filtrada) == 0:
+        print("No se encontraron países")
+        return
+
+    print("\nPaíses filtrados:")
+    print("---------------------------")
+
+    for pais in lista_filtrada:
+        print(f"Nombre: {pais['nombre'].capitalize()}")
+        print(f"Población: {pais['poblacion']}")
+        print(f"Superficie: {pais['superficie']}")
+        print(f"Continente: {pais['continente'].capitalize()}")
+        print("---------------------------")
+
+
+# funcion para ordenar paises (5)
+def ordenar_paises():
+    print("\n------ Ordenar Países ------")
+    print("1. Ordenar por nombre (A-Z)")
+    print("2. Ordenar por población (menor a mayor)")
+    print("3. Ordenar por superficie (menor a mayor)")
+    print("4. Volver al menú")
+
+    opcion = input("Seleccione una opción: ").strip()
+    if opcion not in ["1", "2", "3"]:
+        return
+    
+    lista = obtener_datos()
+    long_lista = len(lista)
+
+    # Ciclo para ordenar 
+    for i in range(long_lista):
+        for j in range(long_lista - 1):
+            
+            if opcion == "1":  #nombre
+                if lista[j]["nombre"] > lista[j + 1]["nombre"]:
+                    temporal = lista[j]
+                    lista[j] = lista[j + 1]
+                    lista[j + 1] = temporal
+            
+            elif opcion == "2":  #población
+                if lista[j]["poblacion"] > lista[j + 1]["poblacion"]:
+                    temporal = lista[j]
+                    lista[j] = lista[j + 1]
+                    lista[j + 1] = temporal
+            
+            elif opcion == "3":  # superficie
+                if lista[j]["superficie"] > lista[j + 1]["superficie"]:
+                    temporal = lista[j]
+                    lista[j] = lista[j + 1]
+                    lista[j + 1] = temporal
+
+    # Mostrar ordenados
+    print("\nPaíses ordenados:")
+    print("---------------------------")
+
+    for pais in lista:
+        print(f"Nombre: {pais['nombre'].capitalize()}")
+        print(f"Población: {pais['poblacion']}")
+        print(f"Superficie: {pais['superficie']} km²")
+        print(f"Continente: {pais['continente'].capitalize()}")
+        print("---------------------------")
+
+
+# funcion para mostrar estadisticas (6)
+def mostrar_estadisticas():
+    print("\n------ Estadísticas de Países ------")
+    lista = obtener_datos()
+
+    if len(lista) == 0:
+        print("No encontramos países registrados")
+        return
+    
+
+    total_paises = len(lista)
+    total_poblacion = 0
+    total_superficie = 0
+
+    mayor_poblacion = lista[0]
+    mayor_superficie = lista[0]
+ 
+    for pais in lista:   # recorremos la lista para calcular mayores y totales
+        total_poblacion += pais["poblacion"]
+        total_superficie += pais["superficie"]
+
+        if pais["poblacion"] > mayor_poblacion["poblacion"]:
+            mayor_poblacion = pais
+
+        if pais["superficie"] > mayor_superficie["superficie"]:
+            mayor_superficie = pais
+    # calculo de promedios
+    promedio_poblacion = total_poblacion // total_paises
+    promedio_superficie = total_superficie // total_paises
+
+    print(f"Total de países: {total_paises}")
+    print(f"Población total: {total_poblacion}")
+    print(f"Población promedio: {promedio_poblacion}")
+    print(f"Superficie total: {total_superficie} km²")
+    print(f"Superficie promedio: {promedio_superficie} km²")
+    print(f"País con mayor población: {mayor_poblacion['nombre'].capitalize()} ({mayor_poblacion['poblacion']})")
+    print(f"País con mayor superficie: {mayor_superficie['nombre'].capitalize()} ({mayor_superficie['superficie']} km²)")
 
 
 
@@ -151,13 +346,13 @@ def mostrar_menu():
             case "2":
                 actualizar_datos()
             case "3":
-                break#buscar_paises()
+                buscar_paises()
             case "4":
-                break#filtrar_paises()
+                filtrar_paises()
             case "5":
-                break#ordenar_paises()
+                ordenar_paises()
             case "6":
-                break#mostrar_estadisticas()
+                mostrar_estadisticas()
             case "7":
                 break#break()
             case __:
